603e95eabd056241c847a9e1f190aa7750b1ea5a
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-17 09:14:48 -0300

    Implementing interface driver

--- digitalocean/digitalocean_driver.go
+++ digitalocean/digitalocean_driver.go
@@ -1,6 +1,41 @@
 package digitalocean
 
-type DigitalOceanDriver struct{}
+import (
+	"context"
+
+	"github.com/rancher/kontainer-engine/types"
+)
+
+type DigitalOceanDriver struct {
+	driverCapabilities types.Capabilities
+}
+
+func (*DigitalOceanDriver) GetDriverOptions(ctx context.Context) (*types.DriverFlags, error) {
+
+	return nil, nil
+}
+
+func (*DigitalOceanDriver) GetDriverUpdateOptions(ctx context.Context) (*types.DriverFlags, error) {
+	return nil, nil
+}
+
+func (*DigitalOceanDriver) Create(ctx context.Context, opts *types.DriverOptions, clusterInfo *types.ClusterInfo) (*types.ClusterInfo, error) {
+
+	return nil, nil
+}
+
+func (*DigitalOceanDriver) Update(ctx context.Context, clusterInfo *types.ClusterInfo, opts *types.DriverOptions) (*types.ClusterInfo, error) {
+
+	return nil, nil
+}
+
+func (*DigitalOceanDriver) PostCheck(ctx context.Context, clusterInfo *types.ClusterInfo) (*types.ClusterInfo, error) {
+	return nil, nil
+}
+
+func (*DigitalOceanDriver) Remove(ctx context.Context, clusterInfo *types.ClusterInfo) error {
+	return nil
+}
 
 func test() {
 
