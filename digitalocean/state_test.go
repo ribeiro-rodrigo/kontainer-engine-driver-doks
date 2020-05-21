d149eb8e73ed3ded4338bd40b8d5335fb3156e3f
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-21 14:17:52 -0300

    Implementing TestGetStateFromOptsKeysCamelCase

--- digitalocean/state_test.go
+++ digitalocean/state_test.go
@@ -35,7 +35,7 @@ func TestGetLabelsFromStringSlicePassNilSlice(t *testing.T) {
 	assert.Equal(t, expectedEmptyLabels, labels, "Slice nil expected empty labels")
 }
 
-func TestGetStateFromOpts(t *testing.T) {
+func TestGetStateFromOptsKeysSnakeCase(t *testing.T) {
 
 	const displayName = "cluster-test"
 	const name = "digitalOceanCluster"
@@ -75,6 +75,69 @@ func TestGetStateFromOpts(t *testing.T) {
 			"node-pool-min":   int64(nodePoolMin),
 			"node-pool-max":   int64(nodePoolMax),
 			"node-pool-count": int64(nodePoolCount),
+		},
+	}
+
+	state, err := getStateFromOpts(&driverOptions)
+
+	assert.Nil(t, err, "Not error in getStateFromOpts")
+	assert.Equal(t, displayName, state.DisplayName, "DisplayName equals")
+	assert.Equal(t, name, state.Name, "Name equals")
+	assert.Equal(t, regionSlug, state.RegionSlug, "RegionSlug equals")
+	assert.Equal(t, versionSlug, state.VersionSlug, "VersionSlug equals")
+	assert.Equal(t, vpcID, state.VPCID, "VPCID equals")
+	assert.Equal(t, nodePoolName, state.NodePool.Name, "nodePoolName equals")
+	assert.Equal(t, nodePoolSize, state.NodePool.Size, "nodePoolSize equals")
+	assert.Equal(t, autoUpgraded, state.AutoUpgrade, "autoUpgraded equals")
+	assert.Equal(t, nodePoolAutoScale, state.NodePool.AutoScale, "nodePoolAutoScale equals")
+	assert.Equal(t, tags, state.Tags, "tags equals")
+	assert.Equal(t, map[string]string{"key1": "label1", "key2": "label2"}, state.NodePool.Labels, "nodePoolLabels equals")
+	assert.Equal(t, nodePoolCount, state.NodePool.Count, "nodePoolCount equals")
+	assert.Equal(t, nodePoolMin, state.NodePool.MinNodes, "nodePoolMin equals")
+	assert.Equal(t, nodePoolMax, state.NodePool.MaxNodes, "nodePoolMax equals")
+
+}
+
+func TestGetStateFromOptsKeysCamelCase(t *testing.T) {
+
+	const displayName = "cluster-test"
+	const name = "digitalOceanCluster"
+	const regionSlug = "nyc3"
+	const versionSlug = "1.17.5-do.0"
+	const vpcID = "18iahdddoaerr"
+	const nodePoolName = "node-pool-1"
+	const nodePoolSize = "s-2vcpu-2gb"
+	const autoUpgraded = true
+	const nodePoolAutoScale = true
+	var tags = []string{"tag1", "tag2"}
+	var nodePoolLabels = []string{"key1=label1", "key2=label2"}
+
+	var nodePoolCount int = 3
+	var nodePoolMin int = 2
+	var nodePoolMax int = 4
+
+	driverOptions := types.DriverOptions{
+		StringOptions: map[string]string{
+			"displayName":  displayName,
+			"name":         name,
+			"regionSlug":   regionSlug,
+			"versionSlug":  versionSlug,
+			"vpcID":        vpcID,
+			"nodePoolName": nodePoolName,
+			"nodePoolSize": nodePoolSize,
+		},
+		BoolOptions: map[string]bool{
+			"autoUpgraded":      autoUpgraded,
+			"nodePoolAutoscale": nodePoolAutoScale,
+		},
+		StringSliceOptions: map[string]*types.StringSlice{
+			"tags":           {Value: tags},
+			"nodePoolLabels": {Value: nodePoolLabels},
+		},
+		IntOptions: map[string]int64{
+			"nodePoolMin":   int64(nodePoolMin),
+			"nodePoolMax":   int64(nodePoolMax),
+			"nodePoolCount": int64(nodePoolCount),
 		},
 	}
 
