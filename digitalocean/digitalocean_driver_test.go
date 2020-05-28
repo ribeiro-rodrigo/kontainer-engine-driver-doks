802d2306ee15f9d20b42e192278f27114ea89249
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-27 22:24:43 -0300

    Implementing TestDriverCreateErrorInDigitalOceanServiceCreate

--- digitalocean/digitalocean_driver_test.go
+++ digitalocean/digitalocean_driver_test.go
@@ -216,6 +216,48 @@ func TestDriverCreateWithoutToken(t *testing.T){
 }
 
 func TestDriverCreateErrorInDigitalOceanServiceCreate(t *testing.T){
+	returnState := state.State{
+		Token:       "a405b7bd3e0d6193f605368102a2deafe4067ed542c92166c6d772fe7e2df019",
+		DisplayName: "cluster-test",
+		Name:        "my-cluster",
+		RegionSlug:  "1.17.5-do.0",
+		NodePool: &godo.KubernetesNodePoolCreateRequest{
+			Name:  "node-pool-1",
+			Size:  "s-2vcpu-2gb",
+			Count: 5,
+		},
+	}
+
+	stateBuilderMock := &StateBuilderMock{
+		buildStateFromOptsMock: func(do *types.DriverOptions) (state.State, error) {
+			return returnState,nil
+		},
+	}
+
+	digitalOceanMock := &DigitalOceanMock{
+		createClusterMock: func(_ context.Context, _ state.State) (string, error) {
+			return "", errors.New("error in create cluster")
+		},
+	}
+
+	driver := Driver{
+		stateBuilder: stateBuilderMock,
+		digitalOceanFactory: func(token string) service.DigitalOcean {return digitalOceanMock},
+	}
+
+	options := &types.DriverOptions{}
+	ctx := context.TODO()
+	clusterInfo := &types.ClusterInfo{}
+
+	stateBuilderMock.On("BuildStateFromOpts",options).Return(returnState)
+	digitalOceanMock.On("CreateCluster", ctx, returnState).Return("",nil)
+
+	_, err := driver.Create(ctx, options , clusterInfo)
+
+	digitalOceanMock.AssertExpectations(t)
+	stateBuilderMock.AssertExpectations(t)
+
+	assert.Error(t, err, "Error in create cluster")
 
 }
 
