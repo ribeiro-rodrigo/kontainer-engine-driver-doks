b800b7bd29d337917ecafbfe7609756a743851b0
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-30 22:28:52 -0300

    Finalize BuildUpdateOptions

--- digitalocean/options/options.go
+++ digitalocean/options/options.go
@@ -134,7 +134,85 @@ func (builderImpl) BuildCreateOptions() *types.DriverFlags {
 }
 
 func BuildUpdateOptions() *types.DriverFlags{
-	return nil
+
+	builder := flagBuilder()
+
+	builder(
+		"name",
+		types.StringType,
+		"Default name to something meaningful to you",
+		nil,
+	)
+
+	builder(
+		"auto-upgraded",
+		types.BoolType,
+		"Automatically updates Kubernetes version",
+		nil,
+	)
+
+	builder(
+		"token",
+		types.StringType,
+		"Personal access token",
+		nil,
+	)
+
+	builder(
+		"version-slug",
+		types.StringType,
+		"Kubernetes version",
+		nil,
+	)
+
+	builder(
+		"tags",
+		types.StringSliceType,
+		"Optional tags to your cluster",
+		nil,
+	)
+
+	builder(
+		"node-pool-labels",
+		types.StringSliceType,
+		"Labels for the node pool",
+		nil,
+	)
+
+	builder(
+		"node-pool-min",
+		types.IntType,
+		"The minimum number of worker nodes",
+		nil,
+	)
+
+	builder(
+		"node-pool-max",
+		types.IntType,
+		"The maximum number of worker nodes",
+		nil,
+	)
+
+	builder(
+		"node-pool-name",
+		types.StringType,
+		"Name of pool of worker nodes",
+		nil,
+	)
+
+	builder(
+		"node-pool-autoscale",
+		types.BoolType,
+		"Enables auto scaling group",
+		nil,
+	)
+
+	return builder(
+		"node-pool-count",
+		types.IntType,
+		"The desired number of worker nodes",
+		nil,
+	)
 }
 
 func flagBuilder() func(name, typ, usage string, def *types.Default) *types.DriverFlags {
